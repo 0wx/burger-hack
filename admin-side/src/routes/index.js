3d1eae4a7840ecf9c8c0d6468aae1d17612e1d7b
0wx <xvnyan@gmail.com>
2023-02-15 02:47:55 +0700

    feat(admin): add home page and protected route

--- admin-side/src/routes/index.js
+++ admin-side/src/routes/index.js
@@ -1,7 +1,17 @@
 import { createBrowserRouter } from 'react-router-dom'
+import ProtectedRoute from '../components/layouts/ProtectedRoute'
+import Home from '../pages/Home'
 import Login from '../pages/Login'
 
 export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <ProtectedRoute>
+        <Home />
+      </ProtectedRoute>
+    ),
+  },
   {
     path: '/login',
     element: <Login />,
