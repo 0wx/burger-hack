ef6f8e7683df1a1219cd3d89e02da310066dc1ae
0wx <xvnyan@gmail.com>
2023-02-15 02:32:32 +0700

    feat(admin): `nosidebar` props in layout and `title` for custom Page Title

--- admin-side/src/components/layouts/MainLayout.jsx
+++ admin-side/src/components/layouts/MainLayout.jsx
@@ -1,12 +1,14 @@
+import { useTitle } from '../../hooks/useTitle'
 import Navbar from './Navbar'
 import Sidebar from './Sidebar'
 
-export default function MainLayout({ children }) {
+export default function MainLayout({ children, nosidebar, title }) {
+  useTitle(title)
   return (
     <>
       <Navbar />
       <div className='pt-16 flex bg-base-100 min-h-screen'>
-        <Sidebar />
+        {!nosidebar && <Sidebar />}
         <div className='py-4 px-10 flex-grow'>{children}</div>
       </div>
     </>
