bd53af9814db57e0d12b45bc9f3bb99354802c91
0wx <xvnyan@gmail.com>
2023-02-15 11:13:49 +0700

    feat(client): using react router and add home and menus routes

---
+++ client-side/src/routes/index.js
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from 'react-router-dom'
+import Home from '../pages/Home'
+import Menu from '../pages/Menu'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/menu',
+    element: <Menu />,
+  },
+])
