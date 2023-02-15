bdb9ca7ec177981910c3d2a47afed3a98132b2bc
0wx <xvnyan@gmail.com>
2023-02-15 09:05:37 +0700

    feat(admin): add dummy login

--- admin-side/src/components/molecules/LoginForm.jsx
+++ admin-side/src/components/molecules/LoginForm.jsx
@@ -1,4 +1,7 @@
 export default function LoginForm() {
+  const handleLogin = () => {
+    localStorage.setItem('access_token', 123)
+  }
   return (
     <div className='flex flex-col max-w-xs gap-4'>
       <input type='text' className='input input-bordered' placeholder='Email' />
@@ -7,7 +10,9 @@ export default function LoginForm() {
         className='input input-bordered'
         placeholder='Password'
       />
-      <button className='btn btn-primary'>Login</button>
+      <button onClick={handleLogin} className='btn btn-primary'>
+        Login
+      </button>
     </div>
   )
 }
