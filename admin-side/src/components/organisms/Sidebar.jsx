277d574c486ba776085aa954b183e053b35814d1
0wx <xvnyan@gmail.com>
2023-02-15 07:28:51 +0700

    feat(admin): add link in the navbar

--- admin-side/src/components/organisms/Sidebar.jsx
+++ admin-side/src/components/organisms/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import HomeIcon from '../atoms/HomeIcon'
 import PuzzleIcon from '../atoms/PuzzleIcon'
 import SidebarButton from '../atoms/SidebarButton'
@@ -6,15 +7,21 @@ import SquarePlusIcon from '../atoms/SquarePlusIcon'
 export default function Sidebar() {
   return (
     <div className='fixed md:static -translate-x-full md:translate-x-0 transition-all p-8 w-60 bg-base-200 flex flex-col gap-2 z-10'>
-      <SidebarButton active>
-        <HomeIcon /> Dashboard
-      </SidebarButton>
-      <SidebarButton>
-        <SquarePlusIcon /> Items
-      </SidebarButton>
-      <SidebarButton>
-        <PuzzleIcon /> Categories
-      </SidebarButton>
+      <Link to='/'>
+        <SidebarButton active>
+          <HomeIcon /> Dashboard
+        </SidebarButton>
+      </Link>
+      <Link to='/items'>
+        <SidebarButton>
+          <SquarePlusIcon /> Items
+        </SidebarButton>
+      </Link>
+      <Link to='/categories'>
+        <SidebarButton>
+          <PuzzleIcon /> Categories
+        </SidebarButton>
+      </Link>
     </div>
   )
 }
