394b66c98b8e9684e048f263889b666542735006
0wx <hai@0wx.dev>
2023-02-17 23:35:43 +0700

    feat(client): categories page

--- client-side/src/pages/Categories.jsx
+++ client-side/src/pages/Categories.jsx
@@ -17,8 +17,32 @@ export default function Categories() {
     }
   }, [dispatch])
 
-  return <MainLayout>
-    <div className='border border-red-500'></div>
-
-  </MainLayout>
+  return (
+    <MainLayout>
+      <div className="flex flex-col items-center justify-center">
+        <div className="text-5xl font-bold">Categories</div>
+        <div className="divider"></div>
+        <div className="w-full max-w-lg rounded-full flex flex-wrap items-center justify-center">
+          {categories.map((cat, i) => {
+            const fontSize = Math.max(
+              Math.min(cat.items.length * 5 + 14, 40),
+              18
+            )
+            return (
+              <div
+                className="px-2 h-min hover:scale-125 transition-all cursor-pointer tooltip"
+                style={{
+                  fontSize,
+                }}
+                data-tip={`${cat.items.length} Items`}
+                key={i}
+              >
+                {cat.name}
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </MainLayout>
+  )
 }
