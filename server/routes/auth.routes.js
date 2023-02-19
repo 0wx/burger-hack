85de384f70824ca2b32977c77b3830c46ff5c565
0wx <hai@0wx.dev>
2023-02-19 15:55:12 +0700

    fix(server): add authorization on register new admin

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,8 +1,17 @@
 const { Router } = require('express')
 const router = Router()
 const UsersController = require('../controllers/users.controller')
+const {
+  authentication,
+  adminAuthorization,
+} = require('../middlewares/auth.middlewares')
 
-router.post('/register', UsersController.register)
+router.post(
+  '/register',
+  authentication,
+  adminAuthorization,
+  UsersController.register
+)
 router.post('/login', UsersController.login)
 
 module.exports = router
