7b99f5a75427c33d2ecaad372629add075bd4910
0wx <hai@0wx.dev>
2023-02-19 17:08:33 +0700

    fix(server): items aliases in users

--- server/models/user.js
+++ server/models/user.js
@@ -1,8 +1,6 @@
-'use strict';
-const { hashSync } = require('bcryptjs');
-const {
-  Model
-} = require('sequelize');
+'use strict'
+const { hashSync } = require('bcryptjs')
+const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -16,78 +14,81 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: 'authorId',
         onDelete: 'cascade',
         onUpdate: 'cascade',
-        
+        as: 'items',
       })
     }
   }
-  User.init({
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notNull: {
-          msg: 'Name is required',
+  User.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'Name is required',
+          },
+          notEmpty: {
+            msg: 'Name is required',
+          },
         },
-        notEmpty: {
-          msg: 'Name is required',
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: {
+          args: true,
+          msg: 'Email already exists',
+        },
+        validate: {
+          notNull: {
+            msg: 'Email is required',
+          },
+          notEmpty: {
+            msg: 'Email is required',
+          },
+          isEmail: {
+            msg: 'Email is invalid',
+          },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'Password is required',
+          },
+          notEmpty: {
+            msg: 'Password is required',
+          },
+        },
+      },
+      role: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'Role is required',
+          },
+          notEmpty: {
+            msg: 'Role is required',
+          },
+          isIn: {
+            args: [['admin', 'customer', 'superadmin']],
+            msg: 'Role must be either admin or customer or superadmin',
+          },
         },
       },
     },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: {
-        args: true,
-        msg: 'Email already exists',
-      },
-      validate: {
-        notNull: {
-          msg: 'Email is required',
-        },
-        notEmpty: {
-          msg: 'Email is required',
-        },
-        isEmail: {
-          msg: 'Email is invalid',
+    {
+      sequelize,
+      modelName: 'User',
+      hooks: {
+        beforeCreate: (user, options) => {
+          user.password = hashSync(user.password, 10)
         },
       },
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notNull: {
-          msg: 'Password is required',
-        },
-        notEmpty: {
-          msg: 'Password is required',
-        },
-      },
-    },
-    role: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notNull: {
-          msg: 'Role is required',
-        },
-        notEmpty: {
-          msg: 'Role is required',
-        },
-        isIn: {
-          args: [['admin', 'customer', 'superadmin']],
-          msg: 'Role must be either admin or customer or superadmin',
-        },
-      },
-    },
-  }, {
-    sequelize,
-    modelName: 'User',
-    hooks: {
-      beforeCreate: (user, options) => {
-        user.password = hashSync(user.password, 10)
-      }
     }
-  });
-  return User;
-};
+  )
+  return User
+}
